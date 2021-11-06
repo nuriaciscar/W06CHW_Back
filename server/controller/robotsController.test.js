ed87eb94571a594fd4349242c1e79d99819849f7
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-11-06 15:05:09 +0100

    Created index.js with port

---
+++ server/controller/robotsController.test.js
@@ -0,0 +1,33 @@
+const Robot = require("../../database/models/robots");
+
+const { getRobots } = require("./robotsController");
+
+jest.mock("../../database/models/robots");
+
+describe("Given a getRobots function", () => {
+  describe("When it receives an object res", () => {
+    test("Then it should invoke the json method", async () => {
+      const robots = [
+        {
+          _id: "1",
+          name: "Wall-e",
+          url: "http://macmagazine.com.br/blog/wp-content/uploads/2008/06/28-walle021.jpg",
+          characteristics: {
+            speed: 2,
+            stamina: 10,
+            creationDate: 2222,
+          },
+        },
+      ];
+
+      Robot.find = jest.fn().mockResolvedValue(robots);
+
+      const res = { json: jest.fn() };
+
+      await getRobots(res, null);
+
+      expect(Robot.find).toHaveBeenCalled();
+      expect(res.json).toHaveBeenCalledWith(robots);
+    });
+  });
+});
