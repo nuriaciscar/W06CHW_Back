cc3b75e88a49408e7430f3fe8b5d1d15080cb451
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-11-06 12:28:40 +0100

    Created robotsSchema and Robot model

---
+++ database/models/robots.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require("mongoose");
+
+const robotsSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  characteristics: {
+    speed: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 10,
+    },
+    stamina: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 10,
+    },
+    creationDate: {
+      type: Number,
+      required: true,
+    },
+  },
+});
+
+const Robot = model("robot", robotsSchema, "robots");
+
+module.exports = Robot;
