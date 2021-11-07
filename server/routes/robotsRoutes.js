ebbb22753c8f51b3b157d4fe861fc1ab4b271a6b
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-11-07 18:52:40 +0100

    Added deleteRobotsById on routes

--- server/routes/robotsRoutes.js
+++ server/routes/robotsRoutes.js
@@ -1,11 +1,16 @@
 const express = require("express");
 require("dotenv").config();
 
-const { getRobots, getRobotsById } = require("../controller/robotsController");
+const {
+  getRobots,
+  getRobotsById,
+  deleteRobotsById,
+} = require("../controller/robotsController");
 
 const router = express.Router();
 
 router.get("/", getRobots);
 router.get("/:idRobot", getRobotsById);
+router.delete("/delete/:idRobot", deleteRobotsById);
 
 module.exports = router;
