94a0ed48370a49eaaf5df30d3423595135c9b462
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-11-09 13:43:36 +0100

    Added error file on middlewares with ValidationError

--- server/routes/loginRoutes.js
+++ server/routes/loginRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const {validate} = require ("express-validation");
 require("dotenv").config();
+
 
 const router = express.Router();
 
 const loginUser = require("../controller/userController");
+const loginRequestSchema = require("../schemas/loginRequestSchema");
 
-router.post("/", loginUser);
+router.post("/", validate(loginRequestSchema), loginUser) => {
+  const {error} = loginRequestSchema.validate (req);
+  error.code = 400;
+};
 
 module.exports = router;
